9bd4c32f9ca5973065db28102558a5e7dc477f3b
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-27 16:54:48 -0300

    Add files via upload

---
+++ variaveis.dart
@@ -0,0 +1,20 @@
+void main (){
+  
+}
+
+/*
+void main (){
+  String nome = 'Gustavo';
+  nome = 'Oliveira';
+  print(nome);
+  //String
+}
+*/
+
+/*
+void main (){
+  int idade = 20;
+  print(idade);
+  //Inteiro
+}
+*/
