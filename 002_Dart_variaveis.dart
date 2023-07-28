afae93304dcd895b543dcead0165fa184a73c84a
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-28 14:53:19 -0300

    Update and rename variaveis.dart to 002_Dart_variaveis.dart

--- 002_Dart_variaveis.dart
+++ 002_Dart_variaveis.dart
@@ -1,20 +1,23 @@
 void main (){
-  
+  bool GustavoDev = true;
+  bool GustavoDev = false;
+  print(GustavoDev);
 }
 
-/*
+void main (){
+  double altura = 1.96;
+  print(altura);
+}
+
 void main (){
   String nome = 'Gustavo';
   nome = 'Oliveira';
   print(nome);
   //String
 }
-*/
 
-/*
 void main (){
   int idade = 20;
   print(idade);
   //Inteiro
 }
-*/
